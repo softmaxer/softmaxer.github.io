4b731ff9613425da833a695130d25fc0928205f0
Sriram Vadlamani <sriram.vadlamani@proton.me>
2024-06-27 23:35:29 +0200

    Add state components to home page to fill it up

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import ProfileButton from "@/components/buttons/profile-button";
 import SocialMediaTray from "@/components/media-tray";
-import Image from "next/image";
-import delorean from "../public/icons/raulxav-Car-delorean.svg";
-import { TimeCircuit } from "@/components/time-circuit";
-import { TimeTravel } from "@/components/time-travel";
+import { Typewriter } from "nextjs-simple-typewriter";
+import React from "react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between items-start gap-14">
+    <div className="flex flex-col items-center gap-10">
       <div className="flex flex-col">
         <p>
           Hey <span>👋</span>! My name is
@@ -22,8 +20,19 @@ export default function Home() {
           </p>
           <SocialMediaTray />
         </div>
+        <div className="flex flex-row gap-4">
+          <span className="font-digital text-xl">CURRENT STATUS:</span>
+          <span className="font-digital text-xl text-green-500">
+            <Typewriter
+              words={["ACTIVE", "Working with a client."]}
+              loop={-1}
+              deleteSpeed={20}
+              typeSpeed={30}
+            />
+          </span>
+        </div>
       </div>
-      <div className="flex flex-row  gap-32 items-center justify-evenly">
+      <div className="flex flex-row items-center gap-10 border border-red-500 rounded-md border-opacity-30 p-5">
         <ProfileButton action="Check out my projects" path="/projects" />
         <ProfileButton action="Download my CV" path="/cv.pdf" />
         <ProfileButton
@@ -31,10 +40,11 @@ export default function Home() {
           path="mailto:sriram.vadlamani@proton.me"
         />
       </div>
-      <div className="flex flex-col">
+      <hr className="border border-t-white w-32 opacity-20" />
+      <div className="flex flex-row gap-1.5">
         <p>Or if you&apos;re feeling adventurous, you can</p>
         <a
-          className="font-bttf text-xl hover:scale-125 transition-all"
+          className="font-bttf text-xl hover:scale-125 hover:text-red-500 transition-all"
           href="#"
         >
           {"<"} time travel
